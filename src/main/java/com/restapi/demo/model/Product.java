4866de1b6d3e7340ac09fda181c554b69eea69ab
ozgur pir <ozgurpir14@gmail.com>
2019-03-23 14:11:32 +0300

    test2

---
+++ src/main/java/com/restapi/demo/model/Product.java
@@ -0,0 +1,25 @@
+package com.restapi.demo.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String description;
+    private String type;
+    private String category;
+}
