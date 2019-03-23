02be548fdad74f99f343adea94365d1750011858
ozgur pir <ozgurpir14@gmail.com>
2019-03-23 13:06:02 +0300

    Project has been started

---
+++ src/main/java/com/restapi/demo/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.restapi.demo.repository;
+
+import com.restapi.demo.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+}
