bb24a78bb7ac8b14e8ba65480827126131b22804
ozgur pir <ozgurpir14@gmail.com>
2019-03-23 14:09:38 +0300

    test1

--- src/main/java/com/restapi/demo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restapi.demo.repository;
-
-import com.restapi.demo.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
